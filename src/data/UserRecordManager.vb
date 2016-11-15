6915e6aebe6c64c6f22777d09186a16d9beb14f3
istama <night.watch.kc@gmail.com>
2016-11-15 13:53:15 +0900

    update UserRecordManager: create method, AddColumnsOfProductivityToRecord()

--- src/data/UserRecordManager.vb
+++ src/data/UserRecordManager.vb
@@ -14,6 +14,8 @@ Imports Common.IO
 Public Class UserRecordManager
   Private ReadOnly properties As ExcelProperties
   
+  Private ReadOnly recordColumnsInfo As UserRecordColumnsInfo
+  
   Private ReadOnly userRecordBuffer As UserRecordBuffer
   Private ReadOnly userRecordLoader As UserRecordLoader
   
@@ -23,6 +25,7 @@ Public Class UserRecordManager
     If properties Is Nothing Then Throw New ArgumentNullException("properties is null")
     
     Me.properties = properties
+    Me.recordColumnsInfo = New UserRecordColumnsInfo(properties)
     Me.userRecordBuffer = New UserRecordBuffer(properties)
     Me.userRecordLoader = New UserRecordLoader(properties, Me.userRecordBuffer)
   End Sub
@@ -38,7 +41,8 @@ Public Class UserRecordManager
     If userInfo Is Nothing Then Throw New ArgumentNullException("userInfo is null")
     Dim record As UserRecord = Me.userRecordBuffer.GetUserRecord(userInfo)
     
-    Return record.GetDailyDataTable(year, month)
+    Return AddColumnOfProductivityToRecord(record.GetDailyDataTable(year, month))
+    'Return record.GetDailyDataTable(year, month)
   End Function
   
   ''' <summary>
@@ -48,7 +52,7 @@ Public Class UserRecordManager
     If UserInfo Is Nothing Then Throw New ArgumentNullException("userInfo is null")
     Dim record As UserRecord = Me.userRecordBuffer.GetUserRecord(userInfo)
     
-    Return record.GetWeeklyDataTableLabelingDate(dateTerm)
+    Return AddColumnOfProductivityToRecord(record.GetWeeklyDataTableLabelingDate(dateTerm))
   End Function
   
   ''' <summary>
@@ -58,7 +62,7 @@ Public Class UserRecordManager
     If UserInfo Is Nothing Then Throw New ArgumentNullException("userInfo is null")
     Dim record As UserRecord = Me.userRecordBuffer.GetUserRecord(userInfo)
     
-    Return record.GetMonthlyDataTableLabelingDate(dateTerm) 
+    Return AddColumnOfProductivityToRecord(record.GetMonthlyDataTableLabelingDate(dateTerm)) 
   End Function
   
   ''' <summary>
@@ -77,24 +81,90 @@ Public Class UserRecordManager
         newTable.Rows.Add(newRow)
       End Sub)
     
-    Return newTable
+    Return AddColumnOfProductivityToRecord(newTable)
   End Function
   
   Public Function GetTotalOfAllUserDailyRecord(dateTerm As DateTerm) As DataTable
     Dim totalRecord As UserRecord = Me.userRecordBuffer.GetTotalRecord
     
-    Return totalRecord.GetDailyDataTableLabelingDate(dateTerm, Function(t) t.BeginDate.Day & "日")
+    Return AddColumnOfProductivityToRecord(totalRecord.GetDailyDataTableLabelingDate(dateTerm, Function(t) t.BeginDate.Day & "日"))
   End Function
   
   Public Function GetTotalOfAllUserWeeklyRecord(dateTerm As DateTerm) As DataTable
     Dim totalRecord As UserRecord = Me.userRecordBuffer.GetTotalRecord
     
-    Return totalRecord.GetWeeklyDataTableLabelingDate(dateTerm)
+    Return AddColumnOfProductivityToRecord(totalRecord.GetWeeklyDataTableLabelingDate(dateTerm))
   End Function
   
   Public Function GetTotalOfAllUserMonthlyRecord(dateTerm As DateTerm) As DataTable
     Dim totalRecord As UserRecord = Me.userRecordBuffer.GetTotalRecord
     
-    Return totalRecord.GetMonthlyDataTableLabelingDate(dateTerm)    
+    Return AddColumnOfProductivityToRecord(totalRecord.GetMonthlyDataTableLabelingDate(dateTerm))    
+  End Function
+  
+  ''' <summary>
+  ''' ユーザデータに生産性の列を追加して返す。
+  ''' </summary>
+  Private Function AddColumnOfProductivityToRecord(table As DataTable) As DataTable
+    Dim newTable As New DataTable
+    
+    ' 新しいテーブルに「名前」もしくは「日にち」の列を追加
+    newTable.Columns.Add(table.Columns(0).ColumnName)
+    
+    ' 新しいテーブルに作業項目の列を追加
+    Dim workItemCnt As Integer = 1
+    For Each item As WorkItemColumnsInfo In Me.recordColumnsInfo.WorkItemList
+      Dim isThereCntColName As Boolean = False
+      Dim isThereTimeColName As Boolean = False
+      If Not String.IsNullOrWhiteSpace(item.WorkCountColName) Then
+        Log.out(item.WorkCountColName)
+        newTable.Columns.Add(item.WorkCountColName)
+        isThereCntColName = True
+      End If
+      If Not String.IsNullOrWhiteSpace(item.WorkTimeColName) Then
+        Log.out(item.WorkTimeColName)
+        newTable.Columns.Add(item.WorkTimeColName)
+        isThereTimeColName = True
+      End If
+      If isThereCntColName AndAlso isThereTimeColName Then
+        Log.out(item.WorkProductivityColName)
+        newTable.Columns.Add(item.WorkProductivityColName)
+      End If
+    Next
+    
+    ' 新しいテーブルに備考の列を追加
+    newTable.Columns.Add(Me.recordColumnsInfo.noteColName)
+    
+    ' データをコピー
+    For Each row As DataRow In table.Rows
+      Dim newRow As DataRow = newTable.NewRow
+      For Each col As DataColumn In table.Columns
+        newRow(col.ColumnName) = row(col.ColumnName)
+      Next
+      
+      ' 生産性を計算
+      For Each item As WorkItemColumnsInfo In Me.recordColumnsInfo.WorkItemList
+        Dim cntColName As String  = item.WorkCountColName
+        Dim timeColName As String = item.WorkTimeColName
+        If cntColName <> String.Empty AndAlso timeColName <> String.Empty Then
+          Dim cntValue As Object = row(cntColName)
+          Dim timeValue As Object = row(timeColName)
+          If Not System.Convert.IsDBNull(cntValue) AndAlso Not System.Convert.IsDBNull(timeValue) Then
+            Dim cnt As Double
+            Dim time As Double
+            If Double.TryParse(DIrectCast(cntValue, String), cnt) AndAlso Double.TryParse(DirectCast(timeValue, String), time) Then
+              If time <> 0 Then
+                Dim productivity As Double = cnt / time
+                newRow(item.WorkProductivityColName) = Math.Round(productivity, 2).ToString
+              End If
+            End If
+          End If
+        End If
+      Next
+      
+      newTable.Rows.Add(newRow)
+    Next
+    
+    Return newTable
   End Function
 End Class
