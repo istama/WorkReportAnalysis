845131966a96111305917f456a05823d6c20907f
istama <night.watch.kc@gmail.com>
2016-12-31 15:42:16 +0900

    refactor UserRecordColumnsInfo

--- src/data/UserRecordColumnsInfo.vb
+++ src/data/UserRecordColumnsInfo.vb
@@ -11,6 +11,9 @@ Imports Common.IO
 ''' レコードの列の情報。
 ''' </summary>
 Public Structure ColumnInfo
+  ''' ダミー用の列  
+  Private Shared ReadOnly _DUMMY As New ColumnInfo(String.Empty, String.Empty, GetType(String), False)
+  
   ''' 列名  
   Public ReadOnly name As String
   ''' 列（Excelでの列）
@@ -20,11 +23,45 @@ Public Structure ColumnInfo
   ''' この列をExcel読み込み時に結果に含めるかどうか
   Private ReadOnly containedToDataTable As Boolean
   
-  Public Sub New(name As String, col As String, type As Type, Optional containedToDataTable As Boolean=True)
+  ''' <summary>
+  ''' ファクトリーメソッド。
+  ''' </summary>
+  Public Shared Function Create(name As String, col As String, type As Type, Optional containedToDataTable As Boolean=True) As ColumnInfo
     If name Is Nothing Then Throw New ArgumentNullException("name is null")
     If col  Is Nothing Then Throw New ArgumentNullException("col is null")
     If type Is Nothing Then Throw New ArgumentNullException("type is null")
     
+    ' 列の名前が空なので例外を投げる
+    If String.IsNullOrWhiteSpace(name) Then Throw New ArgumentException("name is empty")
+    ' 列の値が不正なので例外を投げる
+    If Not Cell.ValidColumn(col)       Then Throw New ArgumentException("col is invalid / " & col)    
+    
+    Return New ColumnInfo(name, col, type, containedToDataTable)
+  End Function
+  
+  ''' <summary>
+  ''' ファクトリーメソッド。
+  ''' Excelを読み込まない列を作成する場合に使用する。
+  ''' </summary>
+  Public Shared Function Create(name As String, type As Type, Optional containedToDataTable As Boolean=True) As ColumnInfo
+    If name Is Nothing Then Throw New ArgumentNullException("name is null")
+    If type Is Nothing Then Throw New ArgumentNullException("type is null")
+    
+    ' 列の名前が空なので例外を投げる
+    If String.IsNullOrWhiteSpace(name) Then Throw New ArgumentException("name is empty")
+    
+    Return New ColumnInfo(name, String.Empty, type, containedToDataTable)    
+  End Function
+  
+  ''' <summary>
+  ''' ファクトリーメソッド。
+  ''' ダミー用の列情報を作成する。
+  ''' </summary>
+  Public Shared Function Dummy() As ColumnInfo
+    Return _DUMMY  
+  End Function
+  
+  Private Sub New(name As String, col As String, type As Type, Optional containedToDataTable As Boolean=True)
     Me.name = name
     Me.col  = col
     Me.type = type
@@ -33,6 +70,7 @@ Public Structure ColumnInfo
   
   ''' <summary>
   ''' 列を作成する。
+  ''' ダミー用の列の場合はnullを返す。
   ''' </summary>
   Public Function CreateDataColumn() As DataColumn
     Return ColumnInfo.CreateDataColumn(Me.name, Me.type)
@@ -42,23 +80,112 @@ Public Structure ColumnInfo
   ''' 列を作成する。
   ''' </summary>
   Public Shared Function CreateDataColumn(name As String, type As Type) As DataColumn
-    Dim col As New DataColumn
-    col.ColumnName = name
-    col.AutoIncrement = False
-    col.DataType = type
-		
-		Return col
+    If String.IsNullOrWhiteSpace(name) OrElse type Is Nothing Then
+      Return Nothing
+    Else
+      Dim col As New DataColumn
+      col.ColumnName    = name
+      col.AutoIncrement = False
+      col.DataType      = type
+  		
+  		Return col     
+    End If
   End Function
   
   ''' <summary>
   ''' Excelを読み込むための列ノードを作成する。
-  ''' ただし、コンストラクタに渡した列か列名が空文字の場合はnullを返す。
+  ''' 列情報が完全でない場合はnullを返す。
   ''' </summary> 
   Public Function CreateExcelColumnNode() As Nullable(Of ExcelColumnNode)
-    If col <> String.Empty AndAlso name <> String.Empty Then
-      Return New ExcelColumnNode(col, name, containedToDataTable)
-    Else
+    If col = String.Empty Then
       Return Nothing
+    Else
+      Return New ExcelColumnNode(col, name, containedToDataTable)  
+    End If
+  End Function
+End Structure
+
+''' <summary>
+''' １つの作業項目あたりの列名。
+''' </summary>
+Public Structure WorkItemColumnsInfo
+  Public Const WORKCOUNT_COL_NAME        As String = "件数"
+  Public Const WORKTIME_COL_NAME         As String = "作業時間"
+  Public Const WORKPRODUCTIVITY_COL_NAME As String = "生産性"
+  
+  ''' 作業件数の列情報
+  Public ReadOnly WorkCountColInfo        As ColumnInfo
+  ''' 作業時間の列情報
+  Public ReadOnly WorkTimeColInfo         As ColumnInfo
+  ''' 生産性の列情報
+  Public ReadOnly WorkProductivityColInfo As ColumnInfo
+  
+  ''' <summary>
+  ''' ファクトリーメソッド。
+  ''' 作業項目名が空の場合、もしくはExcelの列が１つも設定されていない場合は例外を投げる。
+  ''' </summary>
+  Public Shared Function Create(params As ExcelProperties.WorkItemParams) As WorkItemColumnsInfo
+    If String.IsNullOrWhiteSpace(params.Name)        Then Throw New ArgumentException("Excelプロパティの作業項目名が設定されていません。")
+    If String.IsNullOrWhiteSpace(params.WorkCountCol) AndAlso _
+       String.IsNullOrWhiteSpace(params.WorkTimeCol) Then Throw New ArgumentException("Excelプロパティの列情報が設定されていません。")
+      
+    Return New WorkItemColumnsInfo(params)
+  End Function
+  
+  Private Sub New(params As ExcelProperties.WorkItemParams)
+    ' 作業件数の列情報を作成
+    If params.WorkCountCol <> String.Empty Then
+      Me.WorkCountColInfo =
+        ColumnInfo.Create(
+          params.Name & WORKCOUNT_COL_NAME,
+          params.WorkCountCol,
+          GetType(Integer))
+    Else
+      Me.WorkCountColInfo = ColumnInfo.Dummy
+    End If
+    
+    ' 作業時間の列情報を作成
+    If params.WorkTimeCol <> String.Empty Then
+      Me.WorkTimeColInfo =
+        ColumnInfo.Create(
+          params.Name & WORKTIME_COL_NAME,
+          params.WorkTimeCol,
+          GetType(Double))
+    Else
+      Me.WorkTimeColInfo = ColumnInfo.Dummy
+    End If
+    
+    ' 生産性の列情報を作成
+    If params.WorkCountCol <> String.Empty AndAlso params.WorkTimeCol <> String.Empty Then
+      Me.WorkProductivityColInfo =
+        ColumnInfo.Create(
+          params.Name & WORKPRODUCTIVITY_COL_NAME,
+          GetType(Double))
+    Else
+      Me.WorkProductivityColInfo = ColumnInfo.Dummy
+    End If
+  End Sub
+  
+  ''' <summary>
+  ''' Excelの列ノードのツリーを作成する。
+  ''' </summary>
+  Public Function CreateExcelColumnNodeTree() As ExcelColumnNode
+    ' 作業件数と作業時間の列情報を生成
+    Dim cntNode  As Nullable(Of ExcelColumnNode) = Me.WorkCountColInfo.CreateExcelColumnNode()
+    Dim timeNode As Nullable(Of ExcelColumnNode) = Me.WorkTimeColInfo.CreateExcelColumnNode()
+    
+    ' 列情報が有効な場合、親子関係に接続して返す。
+    If cntNode.HasValue Then
+      If timeNode.HasValue Then
+        cntNode.Value.AddChild(timeNode.Value)
+      End If
+      
+      Return cntNode.Value
+    ElseIf timeNode.HasValue
+      Return timeNode.Value
+    Else
+      ' 通常起こらないエラー
+      Throw New InvalidOperationException("オブジェクトの状態が不正です。作業項目の列情報が１つも設定されていません。")
     End If
   End Function
 End Structure
@@ -81,13 +208,34 @@ Public Structure UserRecordColumnsInfo
   ''' 作業項目の列情報のリスト
   Private workItemList As List(Of WorkItemColumnsInfo)
   
-  Public Sub New(properties As ExcelProperties)
+  ''' <summary>
+  ''' ファクトリーメソッド。
+  ''' </summary>
+  Public Shared Function Create(properties As ExcelProperties) As UserRecordColumnsInfo
+    If properties Is Nothing Then Throw New ArgumentNullException("properties is null")
+    
+    If String.IsNullOrWhiteSpace(properties.NoteName) Then
+      Throw New ArgumentException("Excelプロパティファイルの NoteName が設定されていません。")
+    ElseIf String.IsNullOrWhiteSpace(properties.NoteCol)
+      Throw New ArgumentException(
+        "Excelプロパティファイルの NoteCol が設定されていません。" & vbCrLf &
+        "NoteCol とは備考欄の列のことです。")
+    ElseIf String.IsNullOrWhiteSpace(properties.WorkDayCol)
+      Throw New ArgumentException(
+        "Excelプロパティファイルの WorkDayCol が設定されていません。" & vbCrLf &
+        "WorkDayCol とは出勤日の列のことです。")
+    End If
+    
+    Return New UserRecordColumnsInfo(properties)
+  End Function
+  
+  Private Sub New(properties As ExcelProperties)
     If properties Is Nothing Then Throw New NullReferenceException("properties is null")
     
     Me.properties = properties
     
-    Me.noteColInfo = New ColumnInfo(properties.NoteName, properties.NoteCol, GetType(String))
-    Me.workDayColInfo = New ColumnInfo(WORKDAY_COL_NAME, properties.WorkDayCol, GetType(String), False)
+    Me.noteColInfo    = ColumnInfo.Create(properties.NoteName, properties.NoteCol,    GetType(String))
+    Me.workDayColInfo = ColumnInfo.Create(WORKDAY_COL_NAME,    properties.WorkDayCol, GetType(String), False)
   End Sub
   
   ''' <summary>
@@ -98,9 +246,7 @@ Public Structure UserRecordColumnsInfo
     If Me.workItemList Is Nothing Then
       Dim items As IEnumerable(Of WorkItemColumnsInfo) =
         Me.properties.GetWorkItemParamsEnumerable().
-        Select(Function(params) WorkItemColumnsInfo.Create(params)).
-        Where(Function(colInfo) colInfo.HasValue).
-        Select(Function(colInfo) colInfo.Value)
+        Select(Function(params) WorkItemColumnsInfo.Create(params))
       
       Me.workItemList = New List(Of WorkItemColumnsInfo)(items)
     End If
@@ -180,95 +326,3 @@ Public Structure UserRecordColumnsInfo
   End Function
 End Structure
 
-''' <summary>
-''' １つの作業項目あたりの列名。
-''' </summary>
-Public Structure WorkItemColumnsInfo
-  Public Const WORKCOUNT_COL_NAME        As String = "件数"
-  Public Const WORKTIME_COL_NAME         As String = "作業時間"
-  Public Const WORKPRODUCTIVITY_COL_NAME As String = "生産性"
-  
-  ''' 作業件数の列情報
-  Public ReadOnly WorkCountColInfo        As ColumnInfo
-  ''' 作業時間の列情報
-  Public ReadOnly WorkTimeColInfo         As ColumnInfo
-  ''' 生産性の列情報
-  Public ReadOnly WorkProductivityColInfo As ColumnInfo
-  
-  Private Sub New(params As ExcelProperties.WorkItemParams)
-    ' 作業件数の列情報を作成
-    If params.WorkCountCol <> String.Empty Then
-      Me.WorkCountColInfo =
-        New ColumnInfo(
-          params.Name & WORKCOUNT_COL_NAME,
-          params.WorkCountCol,
-          GetType(Integer))
-    Else
-      Me.WorkCountColInfo =
-        New ColumnInfo(
-          String.Empty,
-          String.Empty,
-          GetType(Integer))
-    End If
-    
-    ' 作業時間の列情報を作成
-    If params.WorkTimeCol <> String.Empty Then
-      Me.WorkTimeColInfo =
-        New ColumnInfo(
-          params.Name & WORKTIME_COL_NAME,
-          params.WorkTimeCol,
-          GetType(Double))
-    Else
-      Me.WorkTimeColInfo =
-        New ColumnInfo(
-          String.Empty,
-          String.Empty,
-          GetType(Double))
-    End If
-    
-    ' 生産性の列情報を作成
-    If params.WorkCountCol <> String.Empty AndAlso params.WorkTimeCol <> String.Empty Then
-      Me.WorkProductivityColInfo =
-        New ColumnInfo(
-          params.Name & WORKPRODUCTIVITY_COL_NAME,
-          String.Empty,
-          GetType(Double))
-    Else
-      Me.WorkProductivityColInfo =
-        New ColumnInfo(
-          String.Empty,
-          String.Empty,
-          GetType(Double))
-    End If
-  End Sub
-  
-  Public Shared Function Create(params As ExcelProperties.WorkItemParams) As Nullable(Of WorkItemColumnsInfo)
-    If params.Name Is Nothing OrElse params.Name = String.Empty Then
-      Return Nothing
-    Else
-      Return New WorkItemColumnsInfo(params)
-    End If
-  End Function
-  
-  ''' <summary>
-  ''' Excelの列ノードのツリーを作成する。
-  ''' </summary>
-  Public Function CreateExcelColumnNodeTree() As Nullable(Of ExcelColumnNode)
-    ' 作業件数と作業時間の列情報を生成
-    Dim cntNode  As Nullable(Of ExcelColumnNode) = Me.WorkCountColInfo.CreateExcelColumnNode()
-    Dim timeNode As Nullable(Of ExcelColumnNode) = Me.WorkTimeColInfo.CreateExcelColumnNode()
-    
-    ' 列情報が有効な場合、親子関係に接続して返す。
-    If cntNode.HasValue Then
-      If timeNode.HasValue Then
-        cntNode.Value.AddChild(timeNode.Value)
-      End If
-      
-      Return cntNode
-    ElseIf timeNode.HasValue
-      Return timeNode
-    Else
-      Return Nothing  
-    End If
-  End Function
-End Structure
---
+++ test/data/TestUserRecordColumnsInfo.vb
@@ -0,0 +1,292 @@
+﻿'
+' 日付: 2016/12/30
+'
+Imports NUnit.Framework
+
+Imports System.Data
+Imports Common.COM
+
+<TestFixture> _
+Public Class TestColumnInfo
+  
+  <Test> _
+  Public Sub TestCreate
+    '
+    ' 正しくコンストラクションされることをテスト
+    '
+    
+    ' 通常のファクトリーメソッドのテスト
+    Dim col As ColumnInfo = ColumnInfo.Create("name", "A", GetType(String), True)
+    Assert.AreEqual("name",          col.name) ' 列名
+    Assert.AreEqual("A",             col.col)  ' 列
+    Assert.AreEqual(GetType(String), col.type) ' 型
+    
+    ' Excelの列を指定しないファクトリーメソッド
+    Dim col2 As ColumnInfo = ColumnInfo.Create("age", GetType(Integer))
+    Assert.AreEqual("age",            col2.name) ' 列名
+    Assert.AreEqual(String.Empty,     col2.col)  ' 列
+    Assert.AreEqual(GetType(Integer), col2.type) ' 型
+    
+    ' ダミー列生成用のファクトリーメソッド
+    Dim col3 As ColumnInfo = ColumnInfo.Dummy
+    Assert.AreEqual(String.Empty,    col3.name) ' 列名
+    Assert.AreEqual(String.Empty,    col3.col)  ' 列
+    Assert.AreEqual(GetType(String), col3.type) ' 型
+    
+    '
+    ' 不正な列名なので例外を投げることをテスト
+    '
+    Try
+      ' 列名が空
+      Dim fail As ColumnInfo = ColumnInfo.Create("", "A", GetType(Integer), False)
+      Assert.Fail("must throw exception")
+    Catch ex As Exception
+    End Try
+    
+    Try
+      ' 列が空
+      Dim fail As ColumnInfo = ColumnInfo.Create("age", "", GetType(Integer), False)
+      Assert.Fail("must throw exception")
+    Catch ex As Exception
+    End Try
+    
+    Try
+      ' 列の値が不正
+      Dim fail As ColumnInfo = ColumnInfo.Create("age", "あ", GetType(Integer), False)
+      Assert.Fail("must throw exception")
+    Catch ex As Exception
+    End Try
+  End Sub
+  
+  <Test> _
+  Public Sub TestCreateDataColumn
+    '
+    ' DataColumnが生成されることをテスト
+    '
+    
+    ' 通常の列
+    Dim dataCol As DataColumn = ColumnInfo.Create("name", "A", GetType(String), True).CreateDataColumn
+    Assert.AreEqual("name",          dataCol.ColumnName)
+    Assert.AreEqual(GetType(String), dataCol.DataType)
+    
+    ' Excelの列を持たない列
+    Dim dataCol2 As DataColumn = ColumnInfo.Create("age", GetType(Integer), False).CreateDataColumn
+    Assert.AreEqual("age",            dataCol2.ColumnName)
+    Assert.AreEqual(GetType(Integer), dataCol2.DataType)
+    
+    ' ダミー列
+    Dim dataCol3 As DataColumn = ColumnInfo.Dummy().CreateDataColumn
+    Assert.AreEqual(Nothing, dataCol3)
+  End Sub
+  
+  <Test>
+  Public Sub TestCreateExcelColumnNode
+    '
+    ' Excelを読み込むためのノードが生成されることをテスト
+    '
+    
+    ' 通常の列
+    Dim node As Nullable(Of ExcelColumnNode) = 
+      ColumnInfo.Create("name", "A", GetType(String), True).CreateExcelColumnNode
+    Assert.AreEqual("name", node.Value.GetName)
+    Assert.AreEqual("A",    node.Value.GetCol)
+    
+    ' Excel列を持たない列
+    Dim node2 As Nullable(Of ExcelColumnNode) =
+      ColumnInfo.Create("name", GetType(Integer), True).CreateExcelColumnNode
+    Assert.AreEqual(Nothing, node2)
+  End Sub
+End Class
+
+<TestFixture> _
+Public Class TestWorkItemColumnsInfo
+  Private params1 As ExcelProperties.WorkItemParams = CreateParams(1, "name",    "A", "B")
+  Private params2 As ExcelProperties.WorkItemParams = CreateParams(2, "age",     "",  "C") 
+  Private params3 As ExcelProperties.WorkItemParams = CreateParams(3, "weight",  "D", "")  
+  Private params4 As ExcelProperties.WorkItemParams = CreateParams(4, "height",  "",  "")
+  Private params5 As ExcelProperties.WorkItemParams = CreateParams(5, "",        "E", "F")
+  
+  Private Function CreateParams(id As Integer, name As String, cntCol As String, timeCol As String) As ExcelProperties.WorkItemParams
+    Return New ExcelProperties.WorkItemParams With {
+      .Id           = id,
+      .name         = name,
+      .WorkCountCol = cntCol,
+      .WorkTimeCol  = timeCol
+    }
+  End Function
+  
+  <Test> _
+  Public Sub TestCreate
+    '
+    ' 正しくコンストラクションされることをテスト
+    '
+    
+    ' 通常の列設定の場合
+    Dim col As WorkItemColumnsInfo = WorkItemColumnsInfo.Create(params1)
+    
+    Dim cnt As ColumnInfo = col.WorkCountColInfo
+    Assert.AreEqual("name" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME, cnt.name)
+    Assert.AreEqual("A",                                             cnt.col)
+    Assert.AreEqual(GetType(Integer),                                cnt.type)
+    
+    Dim time As ColumnInfo = col.WorkTimeColInfo
+    Assert.AreEqual("name" & WorkItemColumnsInfo.WORKTIME_COL_NAME, time.name)
+    Assert.AreEqual("B",                                            time.col)
+    Assert.AreEqual(GetType(Double),                                time.type)
+    
+    ' 件数列がない場合
+    Dim col2 As WorkItemColumnsInfo = WorkItemColumnsInfo.Create(params2)
+    
+    Dim cnt2 As ColumnInfo = col2.WorkCountColInfo
+    Assert.AreEqual(ColumnInfo.Dummy, cnt2) ' 列を設定しないとダミー列になる
+    
+    Dim time2 As ColumnInfo = col2.WorkTimeColInfo
+    Assert.AreEqual("age" & WorkItemColumnsInfo.WORKTIME_COL_NAME, time2.name)
+    Assert.AreEqual("C",                                           time2.col)
+    Assert.AreEqual(GetType(Double),                               time2.type)
+    
+    ' 時間列がない場合
+    Dim col3 As WorkItemColumnsInfo = WorkItemColumnsInfo.Create(params3)
+    
+    Dim cnt3 As ColumnInfo = col3.WorkCountColInfo
+    Assert.AreEqual("weight" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME, cnt3.name)
+    Assert.AreEqual("D",                                               cnt3.col)
+    Assert.AreEqual(GetType(Integer),                                  cnt3.type)
+    
+    Dim time3 As ColumnInfo = col3.WorkTimeColInfo
+    Assert.AreEqual(ColumnInfo.Dummy, time3) ' 列を設定しないとダミー列になる
+    
+    ' 列が１つも設定されていない場合
+    Try
+      ' 列情報のないパラメータは不正なので例外を投げる
+      Dim col4 As WorkItemColumnsInfo = WorkItemColumnsInfo.Create(params4)
+      Assert.Fail()
+    Catch ex As Exception
+    End Try
+    
+    ' 列名が設定されていない場合
+    Try
+      ' 列名のないパラメータは不正なので例外を投げる
+      Dim col5 As WorkItemColumnsInfo = WorkItemColumnsInfo.Create(params5)
+      Assert.Fail()
+    Catch ex As Exception
+    End Try
+  End Sub
+  
+  <Test> _
+  Public Sub TestCreateExcelColumnTree
+    '
+    ' Excelの読み込みノードが作成されることをテスト
+    '
+    
+    ' 通常の列の場合
+    Dim node As ExcelColumnNode = WorkItemColumnsInfo.Create(params1).CreateExcelColumnNodeTree
+    Assert.AreEqual("A", node.GetCol)
+    Assert.AreEqual(1,   node.GetChilds.Count)
+    Assert.AreEqual("B", node.GetChilds(0).GetCol)
+    
+    ' 作業列のない場合
+    Dim node2 As ExcelColumnNode = WorkItemColumnsInfo.Create(params2).CreateExcelColumnNodeTree
+    Assert.AreEqual("C", node2.GetCol)
+    Assert.AreEqual(0,   node2.GetChilds.Count)   
+    
+    ' 時間列のない場合
+    Dim node3 As ExcelColumnNode = WorkItemColumnsInfo.Create(params3).CreateExcelColumnNodeTree
+    Assert.AreEqual("D", node3.GetCol)
+    Assert.AreEqual(0,   node3.GetChilds.Count) 
+  End Sub
+End Class
+
+<TestFixture> _
+Public Class TestUserRecordColumnsInfo
+  Private properties As New ExcelProperties("test/ExcelForTest.properties")  
+  
+  <Test> _
+  Public Sub TestConstructor
+    Dim col As UserRecordColumnsInfo = UserRecordColumnsInfo.Create(properties)
+    
+    '
+    ' 列の設定ファイルから列情報オブジェクトが正しくコンストラクションされることをテスト
+    '
+    Assert.AreEqual("Y", col.noteColInfo.col)
+    Assert.AreEqual("Z", col.workDayColInfo.col)
+    
+    Dim l As New List(Of WorkItemColumnsInfo)(col.WorkItems)
+    Assert.AreEqual("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME,        l(0).WorkCountColInfo.name)
+    Assert.AreEqual("item1" & WorkItemColumnsInfo.WORKTIME_COL_NAME,         l(0).WorkTimeColInfo.name)
+    Assert.AreEqual("item1" & WorkItemColumnsInfo.WORKPRODUCTIVITY_COL_NAME, l(0).WorkProductivityColInfo.name)
+    
+    Assert.AreEqual("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME,        l(1).WorkCountColInfo.name)
+    Assert.AreEqual("item2" & WorkItemColumnsInfo.WORKTIME_COL_NAME,         l(1).WorkTimeColInfo.name)
+    Assert.AreEqual("item2" & WorkItemColumnsInfo.WORKPRODUCTIVITY_COL_NAME, l(1).WorkProductivityColInfo.name)
+    
+    Assert.AreEqual("item3" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME,        l(2).WorkCountColInfo.name)
+    Assert.AreEqual(String.Empty,                                            l(2).WorkTimeColInfo.name)
+    Assert.AreEqual(String.Empty,                                            l(2).WorkProductivityColInfo.name)
+    
+    Assert.AreEqual(String.Empty,                                            l(3).WorkCountColInfo.name)
+    Assert.AreEqual("item4" & WorkItemColumnsInfo.WORKTIME_COL_NAME,         l(3).WorkTimeColInfo.name)
+    Assert.AreEqual(String.Empty,                                            l(3).WorkProductivityColInfo.name)
+  End Sub
+  
+  <Test> _
+  Public Sub TestCreateDataTable
+    Dim col As UserRecordColumnsInfo = UserRecordColumnsInfo.Create(properties)
+    
+    '
+    ' 列の設定ファイルからDataTableが作成されることをテスト
+    '
+    Dim table As DataTable = col.CreateDataTable()
+    Dim l     As New List(Of WorkItemColumnsInfo)(col.WorkItems)
+    Assert.AreEqual(9, table.Columns.Count)
+    Assert.AreEqual(l(0).WorkCountColInfo.name,        table.Columns(0).ColumnName)
+    Assert.AreEqual(l(0).WorkTimeColInfo.name,         table.Columns(1).ColumnName)
+    Assert.AreEqual(l(0).WorkProductivityColInfo.name, table.Columns(2).ColumnName) ' 件数と時間の列がある作業項目のみ生産性の列が生成される
+    Assert.AreEqual(l(1).WorkCountColInfo.name,        table.Columns(3).ColumnName)
+    Assert.AreEqual(l(1).WorkTimeColInfo.name,         table.Columns(4).ColumnName)
+    Assert.AreEqual(l(1).WorkProductivityColInfo.name, table.Columns(5).ColumnName)
+    Assert.AreEqual(l(2).WorkCountColInfo.name,        table.Columns(6).ColumnName)
+    Assert.AreEqual(l(3).WorkTimeColInfo.name,         table.Columns(7).ColumnName)
+    Assert.AreEqual(col.noteColInfo.name,              table.Columns(8).ColumnName)
+    
+    '
+    ' DataTableの１列目に指定した名前の列を追加して生成する
+    '
+    Dim table2 As DataTable = col.CreateDataTable("NewColumn")
+    Dim l2     As New List(Of WorkItemColumnsInfo)(col.WorkItems)
+    Assert.AreEqual(10, table2.Columns.Count)
+    Assert.AreEqual("NewColumn",                 table2.Columns(0).ColumnName)
+    Assert.AreEqual(l2(0).WorkCountColInfo.name, table2.Columns(1).ColumnName)
+    Assert.AreEqual(col.noteColInfo.name,        table2.Columns(9).ColumnName)
+  End Sub
+  
+  <Test> _
+  Public Sub TestCreateExcelColumnNodeTree
+    Dim col As UserRecordColumnsInfo = UserRecordColumnsInfo.Create(properties)
+    
+    '
+    ' 列の設定からExcelを読み込むノードが作成されることをテスト
+    '
+    Dim node As ExcelColumnNode = col.CreateExcelColumnNodeTree()
+    AssertEqualNode(UserRecordColumnsInfo.WORKDAY_COL_NAME, "Z", node)
+    
+    Dim childs As List(Of ExcelColumnNode) = node.GetChilds()
+    Dim CNT  As String = WorkItemColumnsInfo.WORKCOUNT_COL_NAME
+    Dim TIME As String = WorkItemColumnsInfo.WORKTIME_COL_NAME
+    AssertEqualNode("item1" & CNT,  "A", childs(0))
+    AssertEqualNode("item1" & TIME, "B", childs(0).GetChilds(0))
+    
+    AssertEqualNode("item2" & CNT,  "C", childs(1))
+    AssertEqualNode("item2" & TIME, "D", childs(1).GetChilds(0))
+    
+    AssertEqualNode("item3" & CNT,  "E", childs(2))
+    AssertEqualNode("item4" & TIME, "F", childs(3))
+    
+    AssertEqualNode("Note",         "Y", childs(4))
+  End Sub
+  
+  Private Sub AssertEqualNode(name As String, col As String, node As ExcelColumnNode)
+    Assert.AreEqual(name, node.GetName)
+    Assert.AreEqual(col,  node.GetCol)
+  End Sub
+End Class
