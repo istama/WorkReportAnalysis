8ba0b24aa718bd40e2897dfea49973d888917b37
istama <night.watch.kc@gmail.com>
2017-01-02 17:15:44 +0900

    refactor UserRecordBuffer

--- src/data/UserRecordBuffer.vb
+++ src/data/UserRecordBuffer.vb
@@ -29,14 +29,19 @@ Public Class UserRecordBuffer
   ''' 集計テーブルに加算したユーザのリスト
   Private ReadOnly addedUserListToTotalRecord As New List(Of String)
   
+  Private ReadOnly dataDateTerm As DateTerm
+  
   Public Sub New(properties As ExcelProperties)
     If properties Is Nothing Then Throw New ArgumentNullException("properties is null")
     
-    Me.properties = properties
-    Me.recordColumnsInfo = New UserRecordColumnsInfo(properties)
+    Me.properties        = properties
+    Me.recordColumnsInfo = UserRecordColumnsInfo.Create(properties)
+    Me.dataDateTerm      = New DateTerm(properties.BeginDate, properties.EndDate) 
     
-    Me.totalRecord = New UserRecord(New UserInfo("total", "999", "xxx"), Me.recordColumnsInfo, properties)
-    Me.totalRecordExceptedUnfilled = New UserRecord(New UserInfo("total", "999", "xxx"), Me.recordColumnsInfo, properties)
+    ' 全ユーザの集計用レコードを作成
+    Dim dummyUser As New UserInfo("dummy", "999", "xxx") ' このパラメータに意味はない
+    Me.totalRecord                 = New UserRecord(dummyUser, Me.recordColumnsInfo, Me.dataDateTerm)
+    Me.totalRecordExceptedUnfilled = New UserRecord(dummyUser, Me.recordColumnsInfo, Me.dataDateTerm)
   End Sub
   
   ''' <summary>
@@ -46,7 +51,7 @@ Public Class UserRecordBuffer
   Public Function CreateUserRecord(userInfo As UserInfo) As UserRecord
     If userInfo   Is Nothing Then Throw New ArgumentNullException("userInfo is null")
     
-    Return New UserRecord(userInfo, Me.recordColumnsInfo, Me.properties)
+    Return New UserRecord(userInfo, Me.recordColumnsInfo, Me.dataDateTerm)
   End Function
 
   ''' <summary>
@@ -100,7 +105,7 @@ Public Class UserRecordBuffer
   ''' <summary>
   ''' 作業時間が０の作業件数が除かれた集計レコードを取得する。
   ''' </summary>
-  Public Function GetTotalRecordExceptedWorkCountOfZeroWorkTimeIs() As UserRecord
+  Public Function GetTotalRecordExceptedUnfilled() As UserRecord
     Return Me.totalRecordExceptedUnfilled
   End Function
   
@@ -112,7 +117,19 @@ Public Class UserRecordBuffer
   Public Function PlusToTotalRecord(userInfo As UserInfo) As Boolean
     SyncLock Me.addedUserListToTotalRecord
       If Not Me.addedUserListToTotalRecord.Contains(userInfo.GetSimpleId) Then
-        UpdateTotalRecord(userInfo, Sub(totalRow, userRow) totalRow.PlusByDouble(userRow))
+        ' 集計レコードにユーザレコードの値を加算する
+        UpdateRecord(
+          userInfo,
+          Me.totalRecord,
+          Sub(userTable, updatedTable) PlusAllData(updatedTable, userTable))
+        
+        ' 集計レコードにユーザレコードの値を加算する
+        ' ただし、作業時間が０の作業件数は除く
+        UpdateRecord(
+          userInfo,
+          Me.totalRecordExceptedUnfilled,
+          Sub(userTable, updatedTable) PlusAllDataExceptedUnfilled(updatedTable, userTable))
+        
         Me.addedUserListToTotalRecord.Add(userInfo.GetSimpleId)
         Return True
       Else
@@ -123,43 +140,139 @@ Public Class UserRecordBuffer
   
   ''' <summary>
   ''' 集計レコードから減算する。
-  ''' 無事、減算できた場合はTureを返す。
-  ''' まだ加算されていないユーザの場合は何もせずFalseを返す。
+  ''' 無事、減算できた場合はTrueを返す。
+  ''' 存在しないユーザなら何もせずにFalseを返す。
   ''' </summary>
   Public Function MinusToTotalRecord(userInfo As UserInfo) As Boolean
     SyncLock Me.addedUserListToTotalRecord
       If Me.addedUserListToTotalRecord.Contains(userInfo.GetSimpleId) Then
-        ' TODO マイナスが行われていない
-        UpdateTotalRecord(userInfo, Sub(totalRow, userRow) totalRow.MinusByDouble(userRow))
+        ' 集計レコードからユーザレコードの値を減算する
+        UpdateRecord(
+          userInfo,
+          Me.totalRecord,
+          Sub(userTable, updatedTable) MinusAllData(updatedTable, userTable))
+        
+        ' 集計レコードからユーザレコードの値を減算する
+        ' ただし、作業時間が０の作業件数は除く
+        UpdateRecord(
+          userInfo,
+          Me.totalRecordExceptedUnfilled,
+          Sub(userTable, updatedTable) MinusAllDataExceptedUnfilled(updatedTable, userTable))
+        
         Me.addedUserListToTotalRecord.Remove(userInfo.GetSimpleId)
         Return True
       Else
-        Return False      
+        Return False
       End If
     End SyncLock
   End Function
   
   ''' <summary>
-  ''' TODO 見直す
-  ''' 集計レコードを更新する。
+  ''' 同じ列、同じ行にあるデータ同士を加算する。
   ''' </summary>
-  Private Sub UpdateTotalRecord(userInfo As UserInfo, f As Action(Of DataRow, DataRow))
-    If userInfo Is Nothing Then Throw New ArgumentNullException("userInfo is null")
+  Private Sub PlusAllData(addedTable As DataTable, addingTable As DataTable)
+    Dim size As Integer = Math.Min(addedTable.Rows.Count, addingTable.Rows.Count)
+    
+    For idx As Integer = 0 To size - 1
+      addedTable.Rows(idx).Plus(addingTable.Rows(idx))
+    Next
+  End Sub
+  
+  ''' <summary>
+  ''' 同じ列、同じ行にあるデータ同士を加算する。
+  ''' ただし、作業時間が０の作業件数は加算しない。
+  ''' </summary>
+  Private Sub PlusAllDataExceptedUnfilled(addedTable As DataTable, addingTable As DataTable)
+    Dim size As Integer = Math.Min(addedTable.Rows.Count, addingTable.Rows.Count)
+    
+    ' 各行の値を加算する
+    For idx As Integer = 0 To size - 1
+      Dim addedRow  As DataRow = addedTable.Rows(idx)
+      Dim addingRow As DataRow = addingTable.Rows(idx)
+      
+      For Each workItems As WorkItemColumnsInfo In Me.recordColumnsInfo.WorkItems
+        Dim timeColName As String = workItems.WorkTimeColInfo.name
+        
+        If Not String.IsNullOrWhiteSpace(timeColName) Then
+          Dim time As Double = addingRow.GetOrDefault(Of Double)(timeColName, 0.0)
+          ' 作業時間が０以上の場合のみ、作業件数を加算する
+          If time > 0.0 Then
+            addedRow(timeColName) = addedRow.GetOrDefault(Of Double)(timeColName, 0.0) + time
+            
+            Dim cntColName As String = workItems.WorkCountColInfo.name
+            
+            If Not String.IsNullOrWhiteSpace(cntColName) Then
+              addedRow(cntColName) = addedRow.GetOrDefault(Of Integer)(cntColName, 0) + addingRow.GetOrDefault(Of Integer)(cntColName, 0)
+            End If
+          End If
+        End If
+      Next
+    Next
+  End Sub
+  
+  ''' <summary>
+  ''' 同じ列、同じ行にあるデータ同士を減算する。
+  ''' </summary>
+  Private Sub MinusAllData(subtractedTable As DataTable, subtractingTable As DataTable)
+    Dim size As Integer = Math.Min(subtractedTable.Rows.Count, subtractingTable.Rows.Count)
+    
+    For idx As Integer = 0 To size - 1
+      subtractedTable.Rows(idx).Minus(subtractingTable.Rows(idx))
+    Next
+  End Sub
+  
+  ''' <summary>
+  ''' 同じ列、同じ行にあるデータ同士を加算する。
+  ''' ただし、作業時間が０の作業件数は減算しない。
+  ''' </summary>
+  Private Sub MinusAllDataExceptedUnfilled(subtractedTable As DataTable, subtractingTable As DataTable)
+    Dim size As Integer = Math.Min(subtractedTable.Rows.Count, subtractingTable.Rows.Count)
+    
+    ' 各行の値を減算する
+    For idx As Integer = 0 To size - 1
+      Dim subtractedRow  As DataRow = subtractedTable.Rows(idx)
+      Dim subtractingRow As DataRow = subtractingTable.Rows(idx)
+      
+      For Each workItems As WorkItemColumnsInfo In Me.recordColumnsInfo.WorkItems
+        Dim timeColName As String = workItems.WorkTimeColInfo.name
+        
+        If Not String.IsNullOrWhiteSpace(timeColName) Then
+          Dim time As Double = subtractingRow.GetOrDefault(Of Double)(timeColName, 0.0)
+          ' 作業時間が０以上の場合のみ、作業件数を加算する
+          If time > 0.0 Then
+            subtractedRow(timeColName) = subtractedRow.GetOrDefault(Of Double)(timeColName, 0.0) - time
+            
+            Dim cntColName As String  = workItems.WorkCountColInfo.name
+            
+            If Not String.IsNullOrWhiteSpace(cntColName) Then
+              subtractedRow(cntColName) = subtractedRow.GetOrDefault(Of Integer)(cntColName, 0) - subtractingRow.GetOrDefault(Of Integer)(cntColName, 0)
+            End If
+          End If
+        End If
+      Next
+    Next
+  End Sub
+  
+  ''' <summary>
+  ''' 指定したユーザのレコードと指定した更新されるレコードのテーブルを月ごとに呼び出し、
+  ''' コールバック関数に引き渡す。
+  ''' </summary>
+  Private Sub UpdateRecord(userInfo As UserInfo, updatedRecord As UserRecord, update As Action(Of DataTable, DataTable))
+    If userInfo      Is Nothing Then Throw New ArgumentNullException("userInfo is null")
+    If updatedRecord Is Nothing Then Throw New ArgumentNullException("updatedRecord is null")
+    If update        Is Nothing Then Throw NEw ArgumentNullException("update is null")
     
     Dim record As UserRecord = Nothing
+    
     If Me.userRecordDictionary.TryGetValue(userInfo.GetSimpleId, record) Then
-      record.GetRecordDateTerm.MonthlyTerms.ForEach(
-        Sub(term)
-          Dim d As DateTime = term.BeginDate
-          Dim userTable  As DataTable = record.GetDailyDataTableForAMonth(d.Year, d.Month)
-          Dim totalTable As DataTable = Me.totalRecord.GetRecord(d.Month)
-          Dim totalTableE As DataTable = Me.totalRecordExceptedUnfilled.GetRecord(d.Month)
-          
-          userTable.Plus(totalTable, Me.recordColumnsInfo)
-          userTable.PlusExceptingWorkCountOfZerpWorkTimeIs(totalTableE, Me.recordColumnsInfo)
-        End Sub)
+      For Each term As DateTerm In record.GetRecordDateTerm.MonthlyTerms
+        Dim d            As DateTime  = term.BeginDate
+        Dim userTable    As DataTable = record.GetRecord(d.Month)
+        Dim updatedTable As DataTable = updatedRecord.GetRecord(d.Month)
+        update(userTable, updatedTable)
+      Next
     Else
       Throw New ArgumentException("指定したユーザのレコードは存在しません。 / userInfo: " & userInfo.GetName)
-    End If    
+    End If
   End Sub
 End Class
---
+++ test/data/TestUserRecordBuffer.vb
@@ -0,0 +1,227 @@
+﻿'
+' 日付: 2017/01/02
+'
+Imports NUnit.Framework
+
+Imports System.Data
+Imports System.Linq
+Imports Common.Account
+Imports Common.Extensions
+
+<TestFixture> _
+Public Class TestUserRecordBuffer
+  
+  ' ユーザ情報オブジェクトを作成する
+  Private Function CreateUserInfo(name As String, id As String) As UserInfo
+    Return New UserInfo(name, id, "xxx")
+  End Function
+  
+  <Test> _
+  Public Sub TestCreateUserRecord
+    Dim p As New ExcelProperties("test/ExcelForTest.properties")
+    
+    '
+    ' ユーザレコードが作成されることをテスト
+    ' ただし、作成されるだけでバッファオブジェクトに登録はされない
+    '
+    Dim buffer As New UserRecordBuffer(p)
+    Dim user   As UserInfo   = CreateUserInfo("john", "001")
+    Dim record As UserRecord = buffer.CreateUserRecord(user)
+    
+    AssertEqualUser(user, record)
+    Assert.False(buffer.Stored(user))
+  End Sub
+  
+  <Test> _
+  Public Sub TestGetUserRecord
+    Dim p As New ExcelProperties("test/ExcelForTest.properties")
+    
+    '
+    ' ユーザレコードが作成されることをテスト
+    ' 作成されたレコードはバッファに登録される
+    '
+    Dim buffer As New UserRecordBuffer(p)
+    Dim user   As UserInfo   = CreateUserInfo("john", "001")
+    Dim record As UserRecord = buffer.GetUserRecord(user)
+    
+    AssertEqualUser(user, record)
+    Assert.True(buffer.Stored(user))    
+  End Sub
+  
+  <Test> _
+  Public Sub TestGetUserRecordAll
+    Dim p As New ExcelProperties("test/ExcelForTest.properties")
+    
+    '
+    ' 全ユーザのレコードを取得することをテスト
+    '
+    Dim buffer As New UserRecordBuffer(p)
+    Dim users As UserInfo() = { 
+      CreateUserInfo("john",   "001"),
+      CreateUserInfo("paul",   "002"),
+      CreateUserInfo("george", "003")
+    }
+    Array.ForEach(users, Sub(user) buffer.GetUserRecord(user))
+    
+    Dim l As List(Of UserRecord) = buffer.GetUserRecordAll().ToList
+    Assert.AreEqual(3, l.Count)
+    For idx As Integer = 0 To users.Count - 1
+      AssertEqualUser(users(idx), l(idx))  
+    Next
+  End Sub
+  
+  ' ユーザ情報とユーザレコードが同一ユーザのものであるか判定する
+  Private Sub AssertEqualUser(user As UserInfo, record As UserRecord)
+    Assert.AreEqual(user.GetName,      record.GetName)
+    Assert.AreEqual(user.GetSimpleId,  record.GetIdNumber)
+  End Sub
+  
+  <Test> _
+  Public Sub TestPlusToTotalRecord
+    Dim p As New ExcelProperties("test/ExcelForTest.properties")
+    
+    '
+    ' 集計レコードに値が加算されるかテスト
+    '
+    Dim buffer As New UserRecordBuffer(p)
+    
+    Dim user   As UserInfo   = CreateUserInfo("john", "001")
+    Dim record As UserRecord = buffer.GetUserRecord(user)
+    InsertData(record)
+    
+    Dim user2   As UserInfo   = CreateUserInfo("paul", "002")
+    Dim record2 As UserRecord = buffer.GetUserRecord(user2)
+    InsertData(record2)
+    
+    Dim user3   As UserInfo   = CreateUserInfo("george", "003")
+    Dim record3 As UserRecord = buffer.GetUserRecord(user3)
+    InsertData(record3)
+    
+    ' 指定したユーザの値を集計レコードに加算
+    buffer.PlusToTotalRecord(user)
+    Dim total As UserRecord = buffer.GetTotalRecord()
+    Dim table As DataTable  = total.GetRecord(10)
+    Assert.AreEqual(14, table.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    
+    buffer.PlusToTotalRecord(user2)
+    Dim table2 As DataTable = total.GetRecord(11)
+    Assert.AreEqual(46, table2.Rows(11).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    
+    buffer.PlusToTotalRecord(user3)
+    Dim table3 As DataTable = total.GetRecord(12)
+    Assert.AreEqual(576, table3.Rows(15).Field(Of Integer)("item3" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    
+    '
+    ' 作業時間が０の行の値は集計値に加算されてないことをテスト
+    '
+    Dim totalE As UserRecord = buffer.GetTotalRecordExceptedUnfilled()
+    Dim tableE As DataTable  = totalE.GetRecord(10)
+    Assert.AreEqual(54, table.Rows(7).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(0, tableE.Rows(7).GetOrDefault("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME, 0))
+    
+    '
+    ' 1度加算されたユーザは２度加算されないことをテスト
+    '
+    buffer.PlusToTotalRecord(user)
+    Assert.AreEqual(42, table.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(69, table2.Rows(11).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(576, table3.Rows(15).Field(Of Integer)("item3" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+  End Sub
+  
+  <Test> _
+  Public Sub TestMinusToTotalRecord
+    Dim p As New ExcelProperties("test/ExcelForTest.properties")
+    
+    '
+    ' 集計レコードから値が減算されるかテスト
+    '
+    Dim buffer As New UserRecordBuffer(p)
+    
+    Dim user   As UserInfo   = CreateUserInfo("john", "001")
+    Dim record As UserRecord = buffer.GetUserRecord(user)
+    InsertData(record)
+    buffer.PlusToTotalRecord(user)
+    
+    Dim user2   As UserInfo   = CreateUserInfo("paul", "002")
+    Dim record2 As UserRecord = buffer.GetUserRecord(user2)
+    InsertData(record2)
+    buffer.PlusToTotalRecord(user2)
+    
+    Dim user3   As UserInfo   = CreateUserInfo("george", "003")
+    Dim record3 As UserRecord = buffer.GetUserRecord(user3)
+    InsertData(record3)
+    buffer.PlusToTotalRecord(user3)
+    
+    ' 指定したユーザの値を集計レコードに加算
+    Dim total  As UserRecord = buffer.GetTotalRecord()
+    Dim totalE As UserRecord = buffer.GetTotalRecordExceptedUnfilled()
+    
+    Dim table   As DataTable = total.GetRecord(10)
+    Dim tableE  As DataTable = totalE.GetRecord(10)
+    Dim table2  As DataTable = total.GetRecord(11)
+    Dim tableE2 As DataTable = totalE.GetRecord(11)
+     
+    Assert.AreEqual(42,  table.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(42,  tableE.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(123, table2.Rows(29).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(123, tableE2.Rows(29).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    
+    buffer.MinusToTotalRecord(user)
+    Assert.AreEqual(28, table.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(28, tableE.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(82, table2.Rows(29).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(82, tableE2.Rows(29).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    
+    buffer.MinusToTotalRecord(user2)
+    Assert.AreEqual(14, table.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(14, tableE.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(41, table2.Rows(29).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(41, tableE2.Rows(29).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    
+    buffer.MinusToTotalRecord(user3)
+    Assert.AreEqual(0, table.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(0, tableE.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(0, table2.Rows(29).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(0, tableE2.Rows(29).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    
+    '
+    ' １度減算されたユーザは２度減算されないことをテスト
+    '
+    buffer.MinusToTotalRecord(user)
+    Assert.AreEqual(0, table.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(0, tableE.Rows(3).Field(Of Integer)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(0, table2.Rows(29).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+    Assert.AreEqual(0, tableE2.Rows(29).Field(Of Integer)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME))
+  End Sub
+  
+  ''' <summary>
+  ''' データテーブルに仮データをセットする。
+  ''' </summary>
+  Private Sub InsertData(record As UserRecord)
+    For m As Integer = 10 To 12
+      Dim t As DataTable = record.GetRecord(m)
+      For d As Integer = 1 To DateTime.DaysInMonth(2016, m)
+        If d Mod 4 = 0 Then
+          t.Rows(d - 1)("item1" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME) = d + m
+          
+          Dim time As Double = 1.0
+          If d Mod 8 = 0 Then time = 0
+          t.Rows(d - 1)("item1" & WorkItemColumnsInfo.WORKTIME_COL_NAME)  = time
+        End If
+        
+        If d Mod 6 = 0 Then
+          t.Rows(d - 1)("item2" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME) = d + m
+          t.Rows(d - 1)("item2" & WorkItemColumnsInfo.WORKTIME_COL_NAME)  = 1
+        End If
+        
+        If d Mod 8 = 0 Then
+          t.Rows(d - 1)("item3" & WorkItemColumnsInfo.WORKCOUNT_COL_NAME) = d * m
+        End If
+        
+        If d Mod 10 = 0 Then
+          t.Rows(d - 1)("item4" & WorkItemColumnsInfo.WORKTIME_COL_NAME)  = m
+        End If
+      Next
+    Next
+  End Sub
+End Class
